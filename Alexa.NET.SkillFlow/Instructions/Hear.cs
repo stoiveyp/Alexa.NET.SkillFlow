7a59620d9b7dec40275f7742c74a0e82f3bcbfb7
Steven Pears <steven.pears@live.co.uk>
2019-10-06 12:17:55 +0100

    Add hear support

---
+++ Alexa.NET.SkillFlow/Instructions/Hear.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.SkillFlow.Instructions
+{
+    public class Hear:ISceneInstruction
+    {
+        public Hear()
+        {
+            Phrases = new List<string>();
+        }
+
+        public Hear(IEnumerable<string> phrases)
+        {
+            Phrases = new List<string>(phrases);
+        }
+
+        public Hear(params string[] phrases)
+        {
+            Phrases = new List<string>(phrases);
+        }
+
+        public List<string> Phrases { get; set; }
+        public string Type => nameof(Hear);
+        public void Add(ISkillFlowComponent component)
+        {
+            throw this.InvalidComponent(component);
+        }
+    }
+}
