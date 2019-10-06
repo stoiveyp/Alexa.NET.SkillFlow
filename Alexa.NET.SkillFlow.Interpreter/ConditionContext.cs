887b04d0abbcd8643eff60bf9d770d471279a9a9
Steven Pears <steven.pears@live.co.uk>
2019-10-06 14:40:06 +0100

    Seperate conditioncontext

---
+++ Alexa.NET.SkillFlow.Interpreter/ConditionContext.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Alexa.NET.SkillFlow.Conditions;
+
+namespace Alexa.NET.SkillFlow.Interpreter
+{
+    public class ConditionContext
+    {
+        public StringBuilder Remaining { get; set; }
+        public Stack<Value> Values { get; set; }
+
+        public Condition Condition
+        {
+            get
+            {
+                if (Values.Count == 0)
+                {
+                    return new ValueWrapper(new False());
+                }
+
+                if (Values.First() is Condition)
+                {
+                    return Values.First() as Condition;
+                }
+
+                return new ValueWrapper(Values.First());
+            }
+        }
+
+        public bool Finished => Remaining.Length == 0;
+
+        public ConditionContext(string condition)
+        {
+            Remaining = new StringBuilder(condition);
+            Values = new Stack<Value>();
+        }
+    }
+}
--- Alexa.NET.SkillFlow.Interpreter/ConditionParser.cs
+++ Alexa.NET.SkillFlow.Interpreter/ConditionParser.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Alexa.NET.SkillFlow.Conditions;
 
 namespace Alexa.NET.SkillFlow.Interpreter
@@ -23,38 +20,6 @@ namespace Alexa.NET.SkillFlow.Interpreter
             }
 
             return context.Condition;
-        }
-    }
-
-    public class ConditionContext
-    {
-        public StringBuilder Remaining { get; set; }
-        public Stack<Value> Values { get; set; }
-
-        public Condition Condition
-        {
-            get
-            {
-                if (Values.Count == 0)
-                {
-                    return new ValueWrapper(new False());
-                }
-
-                if (Values.First() is Condition)
-                {
-                    return Values.First() as Condition;
-                }
-
-                return new ValueWrapper(Values.First());
-            }
-        }
-
-        public bool Finished => Remaining.Length == 0;
-
-        public ConditionContext(string condition)
-        {
-            Remaining = new StringBuilder(condition);
-            Values = new Stack<Value>();
         }
     }
 }
