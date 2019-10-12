e74b1df5593e36dd01d102b46aa287b7f82505eb
Steven Pears <steven.pears@live.co.uk>
2019-10-12 09:45:10 +0100

    Add clear support

---
+++ Alexa.NET.SkillFlow/Instructions/ClearAll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.SkillFlow.Instructions
+{
+    public class ClearAll:ISkillFlowComponent
+    {
+        public string Type => nameof(ClearAll);
+        public void Add(ISkillFlowComponent component)
+        {
+            throw this.InvalidComponent(component);
+        }
+    }
+}
